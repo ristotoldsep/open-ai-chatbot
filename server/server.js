9e72967101e18b397116c1f81f94fe97b6439f40
Risto Tõldsep <r.toldsep@gmail.com>
2022-12-27 15:07:51 +0200

    Added live server (fix 4)

--- server/server.js
+++ server/server.js
@@ -13,6 +13,12 @@ const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.json());
 
 app.get("/", async (req, res) => {
