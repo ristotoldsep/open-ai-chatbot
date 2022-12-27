0606329f979eee47f8b493aa65518155c50be432
Risto Tõldsep <r.toldsep@gmail.com>
2022-12-27 14:47:43 +0200

    Added live server

--- client/script.js
+++ client/script.js
@@ -86,15 +86,15 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
+    const response = await fetch("https://ask-ricoboy.onrender.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: data.get("prompt"),
+      }),
+    });
 
     clearInterval(loadInterval)
     messageDiv.innerHTML = " "
